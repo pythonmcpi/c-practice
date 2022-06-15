cdbf1ae8f2f218ed112313af096d272dc901cc33
Pipythonmc <47196755+pythonmcpi@users.noreply.github.com>
2022-06-15 10:12:39 -0700

    2.8: Integer Power

---
+++ 2.8_integer_power.c
@@ -0,0 +1,28 @@
+/* Write a function to raise a number to an integer power, x_to_int_n(x,n) */
+/* Build: g++ -o 2.8_integer_power.exe 2.8_integer_power.c */
+
+#include <stdio.h>
+
+double x_to_int_n(double x, int n);
+
+int main(void) {
+    double x, n;
+    printf("Enter a number: ");
+    scanf("%lf", &x);
+    printf("Enter a power: ");
+    scanf("%lf", &n);
+
+    double y = x_to_int_n(x, n);
+
+    printf("%.2f ** %.2f = %.2f", x, n, y);
+
+    return 0;
+}
+
+double x_to_int_n(double x, int n) {
+    double current = x;
+    for (;n > 1; --n) {
+        current *= x;
+    }
+    return current;
+}
